01eafdb7628ac123935dbe662718424bae15b3a4
evieepy <eviee.py@gmail.com>
2018-09-11 20:09:26 +1000

    Fix cls issue in group command (Misc)

--- modules/misc.py
+++ modules/misc.py
@@ -994,7 +994,7 @@ class Google(metaclass=utils.MetaCog, colour=0xff3728, thumbnail='https://s3-us-
         """
         pass
 
-    @google_.command(name='image', cls=utils.EvieeCommand, aliases=['img'])
+    @google_.command(name='image', aliases=['img'])
     async def google_image(self, ctx, *, query: str):
         """Search for images on Google Image Search.
 
