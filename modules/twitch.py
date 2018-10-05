52c79d9b16b6d896ecbe8951c6cf411f07b3a55c
eviee <evieepy@gmail.com>
2018-10-06 00:39:47 +1000

    Prototype Twitch cog.

--- modules/twitch.py
+++ modules/twitch.py
@@ -1,69 +1,88 @@
 import discord
 from discord.ext import commands
-from twitchio import commands as tcommands
-
-import asyncio
+import twitchio
+import twitchio.commands as tcommands
 
 import utils
 
 
-class Twitch(tcommands.TwitchBot, metaclass=utils.MetaCog):
+class TwitchCog(tcommands.TwitchBot):
+
+    BASE = 'https://api.twitch.tv/helix/webhooks/hub?'
+    HOST = '192.168.1.43'
+    PORT = 6968
 
     def __init__(self, bot):
-        self.dbot = bot
-        super().__init__(irc_token='oauth:aw55sstdy8udfbtxjgdktocdn6mlyn', api_token='test', nick='mysterialpy',
-                         prefix='?', channels=['mysterialpy'])
+        self.bot = bot
+        super().__init__(irc_token='oauth:lisxp5mk5x701wx3myrloa15qrw9yl', client_id='d9ueaxgpwi5ktn3wheykya3tier0ms',
+                         nick='mysterialbot', prefix='>>', initial_channels=['mysterialpy', 'ladymisai'])
 
-        self.loop.create_task(self.start())
-        self.loop.create_task(self.discord_relay())
+    async def on_member_update(self, before: discord.Member, after: discord.Member):
+        if not after.activity:
+            return
+        elif isinstance(before.activity, discord.Streaming):
+            return
+        elif not isinstance(after.activity, discord.Streaming):
+            return
 
-        self.channel = None
-        self.author = None
+        async with self.bot.pool.acquire() as conn:
+            data = await conn.fetchval("""SELECT twitch FROM guilds WHERE id = $1""", before.guild.id)
 
-    async def discord_relay(self):
-        await self.dbot.wait_until_ready()
+        if not data:
+            return
 
-        def check(message):
-            return message.author.id == self.dbot.user.id and message.channel.id == self.channel.id
+        channel = self.bot.get_channel(data)
 
-        while True:
-            message = await self.dbot.wait_for('message', check=check)
+        embed = discord.Embed(title=f'{after.activity.name}', description=f'Watch live: {after.activity.url}')
+        embed.add_field(name='Game', value=after.activity.details)
+        embed.set_author(name=f'{after.display_name}({after.activity.twitch_name})', url=after.activity.url,
+                         icon_url=after.avatar_url)
+        embed.set_thumbnail(url=after.avatar_url)
 
-            message.author = message.guild.get_member(402159684724719617)
-            ctx = await self.dbot.get_context(message, cls=utils.EvieeContext)
-            await self.dbot.invoke(ctx)
+        await channel.send(embed=embed)
 
-    async def on_ready(self):
-        self.channel = self.dbot.get_channel(490950849867284480)
-
-    @tcommands.twitch_command(name='test')
-    async def test(self, ctx):
-        await ctx.send(f'Hello {ctx.author.name}')
-
-    @tcommands.twitch_command(name='points')
-    async def twitch_points(self, ctx):
-        async with self.dbot.pool.acquire() as conn:
-            value = await conn.fetchval("""SELECT points FROM twitch
-                                           WHERE username = $1 AND channel = $2""", ctx.author.name, ctx.channel.name)
-
-        if not value:
-            return await ctx.send(f'{ctx.author.name} has no points!')
-
-        await ctx.send(f'{ctx.author.name} has {value} points!')
-
-    async def event_ready(self):
-        print(f'Twitch Bot Ready | {self.nick}')
-
-    async def event_message(self, message):
-        await self.channel.send(message.content)
-
-        async with self.dbot.pool.acquire() as conn:
-            await conn.execute("""INSERT INTO twitch(username, channel, points) VALUES($1, $2, 1)
-                                  ON CONFLICT (username, "channel")
-                                  DO UPDATE SET points = COALESCE(twitch.points, 0)::int + 1""",
-                               message.author.name, message.channel.name)
-
-        await self.process_commands(message)
-
-    async def on_message(self, message):
+    @commands.command(cls=utils.EvieeCommandGroup)
+    async def twitch(self, ctx):
         pass
+
+    @twitch.command(name='channel', aliases=['announcements', 'announce'])
+    async def twitch_channel(self, ctx, *, channel: discord.TextChannel=None):
+        """Set your Twitch announcements channel.
+
+        Parameters
+        ------------
+        channel: [Optional]
+            The channel to set as your announcement channel. If this is none the channel you run this command
+            will be used.
+        """
+        if channel is None:
+            channel = ctx.channel
+
+        async with self.bot.pool.acquire() as conn:
+            await conn.execute("""UPDATE guilds SET twitch = $1 WHERE guilds.id = $2""", channel.id, ctx.guild.id)
+
+        await ctx.send(f'Your Twitch announcement channel has been set to: {channel.mention}')
+
+    @twitch.command(name='subscribe')
+    async def twitch_subscribe(self, ctx, *, user: discord.Member):
+        pass
+
+    @twitch.command(name='setup')
+    async def twitch_setup(self, ctx, *, channel: str):
+        """Setup your Twitch channel for announcements.
+        """
+        async with self.bot.pool.acquire() as conn:
+            data = await conn.fetchval("""SELECT twitch FROM guilds WHERE id = $1""", ctx.guild.id)
+
+            if not data:
+                return await ctx.send('Your twitch announcement channel has not been setup yet.\n'
+                                      'Please run: `twitch channel` in your desired announcement channel.')
+
+            await conn.execute("""INSERT INTO twitch(uid, channel) VALUES($1, $2)
+                                  ON CONFLICT(uid) DO UPDATE SET channel = $2""", ctx.author.id, channel)
+
+        await ctx.send(f'Thanks. Your Twitch channel has been set to: `{channel}`')
+
+
+def setup(bot):
+    bot.add_cog(TwitchCog(bot))
