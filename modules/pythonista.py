448db12fbaaf651f2cd37f1223ffdbefa1799e03
EvieePy <mysterialpy@gmail.com>
2018-10-16 22:03:09 +1000

    Add RTFM for TwitchIO

--- modules/pythonista.py
+++ modules/pythonista.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import bs4
 
 import utils
 
@@ -10,6 +11,9 @@ class Pythonista(metaclass=utils.MetaCog):
         self.bot = bot
         self.guild_id = 490948346773635102
         self.channel = self.bot.get_channel(491097233765433346)
+        self.rtfm_cache = {}
+
+        self.bot.loop.create_task(self.build_rtfm_cache())
 
     async def on_member_join(self, member):
         if member.guild.id != self.guild_id:
@@ -35,3 +39,53 @@ class Pythonista(metaclass=utils.MetaCog):
         else:
             await ctx.author.add_roles(role, reason='Automatic role command.')
             await ctx.message.add_reaction('❤')
+
+    @commands.command(aliases=['rtfd'])
+    async def rtfm(self, ctx, *, lookup: str):
+
+        matches = utils.fuzzyfinder(lookup, [(a, v) for a, v in self.rtfm_cache.items()], key=lambda t: t[1], lazy=False)[:5]
+        embed = discord.Embed(title='TwitchIO - RTFD')
+
+        if not matches:
+            embed.description = f'Sorry no results were found for `{lookup}`. Try being more specific.\n' \
+                                f'https://twitchio.readthedocs.io/en/rewrite/twitchio.html'
+            return await ctx.send(embed=embed)
+
+        entries = []
+        for m in matches:
+            entry = f'[{m[0]}]({m[1]})'
+            entries.append(entry)
+
+        entries = '\n'.join(entries)
+        embed.description = entries
+
+        await ctx.send(embed=embed)
+
+    async def build_rtfm_cache(self):
+        self.rtfm_cache = {}
+
+        base = 'https://twitchio.readthedocs.io/en/rewrite/twitchio.html'
+        basecom = 'https://twitchio.readthedocs.io/en/rewrite/twitchio.commands.html'
+
+        async with self.bot.session.get(base) as resp:
+            data = await resp.text()
+
+        async with self.bot.session.get(basecom) as resp:
+            commands = await resp.text()
+
+        soup = bs4.BeautifulSoup(data, 'html.parser')
+
+        for a in soup.find_all('a', {'class': 'headerlink'}):
+            self.rtfm_cache[a['href'].replace('#twitchio.', '')] = base + a['href']
+
+        soup = bs4.BeautifulSoup(commands, 'html.parser')
+
+        for a in soup.find_all('a', {'class': 'headerlink'}):
+            self.rtfm_cache[a['href'].replace('#twitchio.ext.commands.', '')] = basecom + a['href']
+
+
+
+
+
+
+
